899ad1678caf56f811447033c48db1702e3f128d
Aj Comfort <weirdbearddev@gmail.com>
2020-05-25 10:33:59 -0400

    Add GameFile and SaveData classes

---
+++ Assets/_Game/Scripts/IO/GameFile.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace SpaceMonkeys.IO
+{
+    public static class GameFile
+    {
+        #region Private Members
+        private static readonly string fileName = @"SaveGame.smd"; // Update name as needed
+        private static readonly string fullPath = Path.Combine(Application.persistentDataPath, fileName);
+        #endregion Private Members
+
+        #region Public Properties
+        public static bool SaveFileExists => File.Exists(fullPath);
+        #endregion Public Properties
+
+        #region Public Methods
+        public static void SaveGame(SaveData data)
+        {
+            // DebugHelper("Save", data); // * temp method used for testing purposes
+
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (FileStream stream = File.Create(fullPath))
+            {
+                formatter.Serialize(stream, data);
+            }
+        }
+        public static SaveData LoadGame()
+        {
+            var data = new SaveData();
+            if (File.Exists(fullPath))
+            {
+                BinaryFormatter formatter = new BinaryFormatter();
+                using (FileStream stream = new FileStream(fullPath, FileMode.Open))
+                {
+                    data = (SaveData)formatter.Deserialize(stream);
+                }
+
+                DebugHelper("Load", data); // * temp method used for testing purposes
+            }
+            else
+            {
+                Debug.Log($"No save file found, reset the game.");
+                data = null;
+            }
+
+            return data;
+        }
+        public static void DeleteSave()
+        {
+            // TODO: make this more robust in the future
+            try
+            {
+                File.Delete(fullPath);
+            }
+            finally { }
+        }
+        #endregion Public Methods
+
+        #region Private Methods
+        private static void DebugHelper(string methodName, SaveData data)
+        {
+            // UnityEngine.Debug.Log($"{methodName}:Zones:Count {data.Zones.Count}");
+            // foreach (var item in data.Zones)
+            // {
+            //     UnityEngine.Debug.Log($"{methodName}:Zone Active: {item.ZoneType} - {item.IsZoneActive}");
+            //     foreach (var q in item.Quests)
+            //     {
+            //         UnityEngine.Debug.Log($"  Quest: {q.Name}, Cxp: {q.CurrentExperience}; Axp: {q.AllTimeExperience}");
+            //     }
+            // }
+        }
+        #endregion Private Methods
+    }
+}
---
+++ Assets/_Game/Scripts/IO/SaveData.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace SpaceMonkeys.IO
+{
+    [System.Serializable]
+    public class SaveData
+    {
+        /* This represents all the data needed for saving/loading the game
+        ** Need to save:
+        */
+        #region Members
+        public float SaveFormat = 0.1f;
+        // TODO add data for saving
+        #endregion Members
+
+        #region Ctors
+        public SaveData() { } // for loading a game
+        // TODO update saving constructor to take parameters for saving
+        public SaveData(int x) // for saving a game
+        {
+            // TODO add code to save game state
+        }
+        #endregion Ctors
+    }
+}
